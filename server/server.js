a0acd39fb3012462ceae1461163beb48de792c88
Lakshmi <bandaru.lakshmi.s.134@gmail.com>
2026-05-20 18:04:33 +0530

    improved cors

--- server/server.js
+++ server/server.js
@@ -59,7 +59,9 @@ MIDDLEWARE
 ========================
 */
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173",
+      process.env.CLIENT_URL
+  ],
   credentials: true
 }));
 
